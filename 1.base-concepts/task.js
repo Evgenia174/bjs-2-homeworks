596931ef9d30023457a921a2ecace9c6ace9178c
Evgenia <evgenya-7495@mail.ru>
2023-11-27 17:56:35 +0500

    Task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+   let d = b**2 - 4 * a * c;
+  if (d < 0) {
+      return [];
+    } else if (d === 0) {
+    arr.push(-b / (2 * a));
+      return arr;
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+     return arr;
+  }
+  solveEquation(1, 2, 3);
 }
-
+  
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent/100/12;
+  let creditBody = amount - contribution;
+  let payment = creditBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  let totalAmount = payment * countMonths;
+  return totalAmount.toFixed(2);
 }
+let result = Number(calculateTotalMortgage(10, 0, 50000, 12));
+console.log(result);
