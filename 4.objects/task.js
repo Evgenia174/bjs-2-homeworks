2462a2c46533b75eaf1ada6ec89435b93d4c98ed
Evgenia <evgenya-7495@mail.ru>
2023-12-11 12:30:05 +0500

    first-try

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks){
+    let addMarks = 0;
+    for (let i = 0; i < marks.length; i++) {
+      if (this.marks === undefined) {
+      console.log("студент отчислен");
+      break;
+    } else {
+      this.marks.push(Number(marks[i]));
+     }
+    }
+    return addMarks;
+  } 
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    if(typeof this.marks === 'undefined' || this.marks.length === 0) {
+      return 0;
+    } else {
+      return this.marks.reduce(function(a, b) { return a + b; 
+    }, 0) / this.marks.length;
+   }
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+  }
