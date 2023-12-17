1965b93f854e0bd63c8863a3ee376dfeef403fc7
Evgenia <evgenya-7495@mail.ru>
2023-12-17 12:11:26 +0500

    third-try

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,77 +1,12 @@
-﻿/*
-////////////// 1 //////////////
-function parseCount(incomingValue) {
-    try {
-      const parsedValue = Number.parseFloat(incomingValue); 
-      if(isNaN(Number.parseFloat(incomingValue))) { 
-       throw new Error("Невалидное значение"); 
-   } return parsedValue; 
-} catch(err) {
-   console.error(err); 
-  } 
-}
-  
-function validateCount(incomingValue) {
-    try {
-      const parsedValue = parseCount(incomingValue);
-      if (typeof parsedValue === "string") {
-        return parsedValue;
-      } else {
-        return parsedValue;
-      }
-    } catch (error) {
-      return "Ошибка при парсинге числа";
-    }
-}
-
-////////// 2 ////////////
-class Triangle {
-    constructor(a, b, c) {
-      this.a = a;
-      this.b = b;
-      this.c = c;
-      if (
-        this.a + this.b < this.c ||
-        this.a + this.c < this.b ||
-        this.b + this.c < this.a
-      ) {
-        throw new Error("Треугольник с такими сторонами не существует");
-      }
-    }
-    get perimetr() {
-      return this.a + this.b + this.c;
-    }
-    get area() {
-        const semiPerimeter = (this.a + this.b + this.c) / 2;
-        return Math.sqrt(semiPerimeter * (semiPerimeter - this.a) * (semiPerimeter - this.b) * (semiPerimeter - this.c));
-        }
-    }
-  
-  function getTriangle(a, b, c) {
-    try {
-      return new Triangle(a, b, c);
-    } catch (error) {
-      return {
-        get area() {
-          return "Ошибка! Треугольник не существует";
-        },
-        get perimetr() {
-          return "Ошибка! Треугольник не существует";
-        },
-      };
-    }
-  }
-*/
-
-function parseCount(incomingValue) {
+﻿function parseCount(incomingValue) {
   try {
     const parsedValue = Number.parseFloat(incomingValue);
-    if (isNaN(Number.parseFloat(incomingValue))) {
+    if (isNaN(parsedValue)) {
       throw new Error("Невалидное значение");
     }
     return parsedValue;
   } catch (error) {
-    return error;
+    throw error;
   }
 }
 
@@ -94,21 +29,37 @@ class Triangle {
     }
   }
 
-  get perimetr() {
-    return this.a + this.b + this.c;
+  get perimeter() {
+    const sidesLengths = this.a + this.b + this.c;
+    return Number(sidesLengths.toFixed(2));
   }
   get area() {
-    const semiPerimeter = this.perimetr / 2;
-    return Math.sqrt(
-      semiPerimeter *
-        (semiPerimeter - this.a) *
-        (semiPerimeter - this.b) *
-        (semiPerimeter - this.c),
-    );
-  }
+    const semiPerimeter = this.perimeter / 2;
+    const area = Math.sqrt(semiPerimeter * (semiPerimeter - this.a) * (semiPerimeter - this.b) * (semiPerimeter - this.c));
+return Number(area.toFixed(2));
 }
+  }
 
-function getTriangle(a, b, c) {
+
+  /////// не пойму почему с Number(.toFixed(2)) выдает ошибку.....
+  
+  /*function getTriangle(a, b, c) {
+    try {
+      const triangle = new Triangle(a, b, c);
+      const area = Number(triangle.area.toFixed(2));
+      const perimeter = Number(triangle.perimeter.toFixed(2));
+      return { area, perimeter };
+    } catch (err) {
+      return {
+        area: "Ошибка! Треугольник не существует",
+        perimeter: "Ошибка! Треугольник не существует",
+      };
+    }
+  }*/
+
+
+
+  function getTriangle(a, b, c) {
   try {
     return new Triangle(a, b, c);
   } catch (error) {
@@ -116,7 +67,7 @@ function getTriangle(a, b, c) {
       get area() {
         return "Ошибка! Треугольник не существует";
       },
-      get perimetr() {
+      get perimeter() {
         return "Ошибка! Треугольник не существует";
       },
     };
